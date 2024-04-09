2e1b46d220dd2de721993b2c915f518da601926f
kokofixcomputers <kokocanfixit@kokofixcomputers.serv00.net>
2024-04-09 12:45:26 -0700

    Add readme generator

---
+++ readme_generator.py
@@ -0,0 +1,22 @@
+import os
+import urllib.parse
+
+
+# Set the directory you want to get the files from
+directory = '/Users/ct/Downloads/extensions-unmerged'
+
+# Get a list of all the files in the directory
+files = os.listdir(directory)
+
+# Open the file in write mode to empty it
+with open('file_names.txt', 'w') as file:
+    pass
+
+# Write the file names to the text file
+with open('file_names.txt', 'a') as file:
+    for filename in files:
+        filename_display = filename
+        if ' ' in filename:
+            # Replace spaces with URL encoding (%20)
+            filename = urllib.parse.quote(filename)
+        file.write(filename_display + ' [url](https://kokofixcomputers.github.io/scratch-extensions/' + filename + ')<br><br>\n')
